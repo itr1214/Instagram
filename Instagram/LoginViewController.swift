5ddd8db5355d8f7cfe62891e23ab664313a1bbc4
ichirou.okada <itr121416@yahoo.co.jp>
2017-01-08 22:48:06 +0900

    Controllerなどを追加

---
+++ Instagram/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  Instagram
+//
+//  Created by 岡田 一郎 on 2017/01/08.
+//  Copyright © 2017年 Ichirou Okada. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+}
